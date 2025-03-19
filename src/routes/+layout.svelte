262a768ec66416770d7d283dd9914fc341b587e7
Hyunbin Seo <47051820+hyunbinseo@users.noreply.github.com>
2025-03-19 16:20:32 +0900

    fix: disable pagefind in dev mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,17 @@
+<script>
+	import { dev } from "$app/environment";
+</script>
+
 <svelte:head>
-	<link href="/pagefind/pagefind-ui.css" rel="stylesheet" />
-	<script src="/pagefind/pagefind-ui.js"></script>
-	<script>
-		window.addEventListener('DOMContentLoaded', (event) => {
-			new PagefindUI({ element: '#search', showSubResults: true });
-		});
-	</script>
+	{#if !dev}
+		<link href="/pagefind/pagefind-ui.css" rel="stylesheet" />
+		<script src="/pagefind/pagefind-ui.js"></script>
+		<script>
+			window.addEventListener('DOMContentLoaded', (event) => {
+				new PagefindUI({ element: '#search', showSubResults: true });
+			});
+		</script>
+	{/if}
 </svelte:head>
 
 <div id="search"></div>
